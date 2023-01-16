4a6e05618a356dcd038fd28fb541e90f4e675514
kirengaboris <kirengaboris5@gmail.com>
2023-01-16 16:54:25 +0200

    fixed all links

---
+++ ui/auth/contact.js
@@ -0,0 +1,15 @@
+// const names = document.getElementById("name");
+// const emails = document.getElementById("email");
+// const message = document.getElementById("message");
+// const contactForm = document.getElementById("contact-form");
+
+// contactForm.addEventListener("submit", e =>{
+//     e.preventDefault();
+
+//     const namesValue = names.value;
+//     const emailsValue = emails.value;
+//     const messageValue = message.value;
+
+//     console.log(namesValue, emailsValue, messageValue);
+// })
+
