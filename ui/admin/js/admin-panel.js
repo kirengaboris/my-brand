e772c94d89dc163e0bc3ff651c8f24879ce628be
kirengaboris <kirengaboris5@gmail.com>
2023-01-17 14:36:03 +0200

    All validations done

--- ui/admin/js/admin-panel.js
+++ ui/admin/js/admin-panel.js
@@ -7,22 +7,42 @@ const adminError3 = document.getElementById("admin-error3");
 const previewBtn = document.getElementById("postbtn");
 const articlesForm = document.getElementById("add-post-form");
 
+
+
 articlesForm.addEventListener("submit", e =>{
     e.preventDefault();
+    var myContent = tinymce.activeEditor.getContent();
+    var topicName = topic.value;
+    var tittleName = title.value;
+   
     
-    if(topic.value == null || topic.value === ""){
+    if(checkBlog(myContent,topicName,tittleName)){
+        location.href = "/ui/admin/preview-blog.html"
+    }
+    
+})
+
+function checkBlog (myContent,topicName,tittleName){
+    let valid = true
+
+    if(topicName == null || topic.value === ""){
         adminError1.innerHTML = "Topic is required";
         topic.classList.add("invalid-article");
+        valid = false;
     }
 
-    if(title.value == null || title.value === ""){
+    if(tittleName == null || title.value === ""){
         adminError2.innerHTML = "Title is required";
         title.classList.add("invalid-article");
+        valid = false;
     }
-
-    if(article.value == null || article.value === ""){
-        adminError3.innerHTML = "Empty blog cannot be posted"
-        article.classList.add("invalid-article");
+    if(myContent == null || myContent === ""){
+        adminError3.innerHTML = "Empty blog cannot be posted" 
+        valid = false;  
     }
-
-})
+    if(myContent.length > 0 && myContent.length < 20){
+        adminError3.innerHTML = "Blog too short"
+        valid = false
+    }
+    return valid;
+}
