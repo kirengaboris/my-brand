a00fc130ed2bba33f9a9426128d0089b6bd1b417
kirengaboris <kirengaboris5@gmail.com>
2023-01-23 00:30:10 +0200

    Added swipers but not working

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-const blogList = document.getElementById("blogs");
+const blogList = document.getElementById("blogList");
 
 let blogsArray = JSON.parse(localStorage.getItem("Blogs"))??[];
+
+let blogContainer = document.createElement("div");
+    blogContainer.classList.add("blog-container");
+    blogContainer.classList.add("swiper-wrapper");
 
 for (let i = 0; i < blogsArray.length; i++) {
     let blog = blogsArray[i];
     let blogCardEl = document.createElement("div");
-    blogCardEl.classList.add("blog1");
+    blogCardEl.classList.add("blog");
+    let singleBlog = document.createElement("div");
+    singleBlog.classList.add("single-blog");
+    singleBlog.classList.add("swipe-slider");
+    
     let blogLink =document.createElement("li")
     blogLink.classList.add("blog-view");
-    blogLink.innerHTML = `<a href="/ui/client/single-blog-view.html#${blog.blogId}" target="_blank">Read More</a>`;
-    blogList.appendChild(blogLink);
     blogCardEl.innerHTML = `<h1>${blog.topic}</h1>
                             <h2>${blog.title}</h2>
                             <p class="teaser">${blog.summary}</p>`;
-    blogList.appendChild(blogCardEl);
+    singleBlog.appendChild(blogCardEl);
+    blogLink.innerHTML = `<a href="/ui/client/single-blog-view.html#${blog.blogId}" target="_blank">Read More</a>`;
+    singleBlog.appendChild(blogLink);
+    blogContainer.appendChild(singleBlog);
 }
+
+blogList.appendChild(blogContainer);
+
 
 // var blogContainerEl = document.getElementById("blog-container");
 // for (var i = 0; i < blogList.length; i++) {
